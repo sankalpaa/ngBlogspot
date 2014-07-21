7740fa7a58bd6f93e5576ab2f81121068a87611c
sankalpa <sab@exilesoft.com>
2014-07-21 18:14:17 +0530

    Filter_InitialCheckin

---
+++ Collection/filterCollection.js
@@ -0,0 +1,6 @@
+﻿var filterTest = angular.module('filterTest', []);
+filterTest.controller('filterTestController',function($scope) {
+	$scope.onlyEmailCom = function (email) {
+		return email.split('@')[1] === 'email.com';
+	};
+})
